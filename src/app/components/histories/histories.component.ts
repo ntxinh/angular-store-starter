6be409cc379802660dd66bd45b8e32c39c23faa9
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2023-09-13 11:36:56 +0700

    Update histories.component.ts

--- src/app/components/histories/histories.component.ts
+++ src/app/components/histories/histories.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { selectHistories } from 'src/app/store/history/history.selectors';
 
 @Component({
   selector: 'app-histories',
   templateUrl: './histories.component.html',
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements OnInit, OnDestroy {
   title = 'Histories';
 
   histories: Array<string> = [];
+  subscription: Subscription;
 
   constructor(private store: Store) {
-    this.store.select(selectHistories).subscribe((histories) => {
+    this.subscription = this.store.select(selectHistories).subscribe((histories) => {
       this.histories = histories;
     });
   }
 
   ngOnInit() {}
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
